57c6e7a9832ce10e2c671f732fb28e5f9c8f2ecd
Kibatun <ifkfvjif@mail.ru>
2023-11-13 03:49:02 +0500

    feat: wpf UI with good data interpretation

---
+++ MyPlugins/CategoryCountViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugins
+{
+    public class CategoryCountViewModel
+    {
+        private string _category;
+        private int _count;
+
+        public string Category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+                OnPropertyChanged(nameof(Category));
+            }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MyPlugins/CountResultsWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPlugins
+{
+    /// <summary>
+    /// Логика взаимодействия для CountResultsWindow.xaml
+    /// </summary>
+    public partial class CountResultsWindow : Window
+    {
+        public ObservableCollection<CategoryCountViewModel> CategoryCounts { get; set; }
+
+        public CountResultsWindow(ObservableCollection<CategoryCountViewModel> categoryCounts)
+        {
+            InitializeComponent();
+            CategoryCounts = categoryCounts;
+            CategoryDataGrid.ItemsSource = CategoryCounts;
+        }
+    }
+}
